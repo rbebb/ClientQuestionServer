fe1a4aab93196390f0e27720c76b3ddbfd8f620d
Ryan Bebb <bebbr@wit.edu>
2017-11-17 09:40:02 -0500

    Added basic GUI components

--- Network Programming Final Project - AskMe/src/final_project/GUI.java
+++ Network Programming Final Project - AskMe/src/final_project/GUI.java
@@ -1,6 +1,8 @@
 package final_project;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
@@ -12,6 +14,18 @@ public class GUI extends Application {
 	
 	public void start(Stage primaryStage) throws Exception {
 		
+//		Program Title
+		primaryStage.setTitle("AskMe");
+		
+		Pane startPane = new Pane();
+//		Size for program window (startPane and gamePane)
+		int paneWidth = 1000;
+		int paneHeight = 700;
+		
+		Scene startScene = new Scene(startPane, paneWidth, paneHeight);
+		primaryStage.setScene(startScene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
 	}
 
 }
