aed404cc2c3d78e6ee7100d6e31642e0ae232533
Ryan Bebb <bebbr@wit.edu>
2017-11-17 09:56:18 -0500

    Trying to implement printing chat messages to GUI

--- Network Programming Final Project - AskMe/src/final_project/GUI.java
+++ Network Programming Final Project - AskMe/src/final_project/GUI.java
@@ -3,6 +3,7 @@ package final_project;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
@@ -21,6 +22,12 @@ public class GUI extends Application {
 //		Size for program window (startPane and gamePane)
 		int paneWidth = 1000;
 		int paneHeight = 700;
+		
+		new QuestionServer();
+		
+		if ()
+		ToGUI chatMessage = new ToGUI();
+		chatMessage.addText(100, 100, QuestionServer.questions.toString(), "Verdana", 20, Color.RED, startPane);
 		
 		Scene startScene = new Scene(startPane, paneWidth, paneHeight);
 		primaryStage.setScene(startScene);
